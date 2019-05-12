b4342e576dd9ed300946c91807da663faa5ba1fd
Khac Man <khacmanle@gmail.com>
2019-05-12 19:29:23 +0800

    refactor fibonacci

--- scheme/fibonacci.scm
+++ scheme/fibonacci.scm
@@ -1,13 +1,13 @@
 #lang scheme
 
 (define (fib x)
-  (define (fibIter a b i n)
-    (if (= i n)
+  (define (fibIter a b i)
+    (if (= i 2)
         (+ a b )
-        (fibIter b (+ a b) (+ i 1) n)))
+        (fibIter b (+ a b) (- i 1))))
   (if (< x 2)
       x
-      (fibIter 0 1 2 x)))
+      (fibIter 0 1 x)))
 
-(fib 9)
+(fib 8)
 
