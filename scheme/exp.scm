9c8e23de5da4139edef2b339af426b42770d3c99
Khac Man <khacmanle@gmail.com>
2019-05-13 22:16:31 +0800

    Refactor exp to use closure

--- scheme/exp.scm
+++ scheme/exp.scm
@@ -1,2 +1,17 @@
 #lang scheme
 
+(define (expR x n)
+  (if (= n 0)
+      1
+      (* x (expR x (- n 1)))))
+(expR 2 10)
+
+(define (exp x n)
+  (define (iter acc a i)
+    (if (= i 0)
+        acc
+        (iter (* acc a)
+              a
+              (- i 1))))
+  (iter 1 x n))
+(exp 2 10)
